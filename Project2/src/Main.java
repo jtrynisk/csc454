b62c769f7ab09a12b0371f0dc48f2103568d04e6
Jondn Tryniski <jtrynisk@oswego.edu>
2019-09-26 11:10:15 -0400

    updated control flow

--- Project2/src/Main.java
+++ Project2/src/Main.java
@@ -4,14 +4,79 @@ public class Main
 {
     public static  void main(String args[])
     {
+        int q = 0, n = 0, d = 0;
+        boolean changeButton;
+        String input, output;
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter the beggining state of the vending machine: ");
-        String input = sc.next();
+        input = sc.next();
         char[] inputArr = input.toCharArray();
+
+
 
         for(int i = 0; i < inputArr.length; i++)
         {
-            
+            if(inputArr[i] == 'q')
+            {
+                q++;
+            }
+            if(inputArr[i] == 'n')
+            {
+                n++;
+            }
+            if(inputArr[i] == 'd')
+            {
+                d++;
+            }
         }
+
+        VendingMachine vm = new VendingMachine(q, n, d, false);
+
+        while(!input.equals("exit"))
+        {
+            //Reset the variables
+            q = n = d = 0;
+            changeButton = false;
+
+            //Ask for input
+            System.out.print("Please enter input: ");
+            input = sc.next();
+            //Exit program if needed.
+            if(input.equals("exit"))
+            {
+                break;
+            }
+            inputArr = input.toCharArray();
+
+
+            //Process the input
+            for(int i = 0; i < inputArr.length; i++)
+            {
+                if(inputArr[i] == 'q')
+                {
+                    q++;
+                }
+                if(inputArr[i] == 'n')
+                {
+                    n++;
+                }
+                if(inputArr[i] == 'd')
+                {
+                    d++;
+                }
+                if(inputArr[i] == 'c')
+                {
+                    changeButton = true;
+                }
+                if(inputArr[i] == 'w')
+                {
+                    //Intentionally left blank
+                }
+            }
+
+            //Have vending machine process the input.
+            vm.processInput(q, n, d, changeButton);
+        }
+
     }
 }
--- Project2/src/VendingMachine.java
+++ Project2/src/VendingMachine.java
@@ -1,8 +1,15 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class VendingMachine
 {
-    private int quarters, nickels, dimes, total;
+    DecimalFormat format = new DecimalFormat("##.##");
+    private int quarters, nickels, dimes, total, numCoffees = 0;
+    private String changeReturned = "", output = "";
     private double totalInserted;
     private boolean changeButton;
+    private boolean coffeeDispensed;
 
     public VendingMachine(int q, int n, int d, boolean changeButton)
     {
@@ -16,73 +23,118 @@ public class VendingMachine
 
     public void processInput(int q, int n, int d, boolean buttonPressed)
     {
+        String output = "";
+        output = lambda();
+        System.out.println(output);
+        delta(q, n, d, buttonPressed);
+    }
+
+    private String lambda()
+    {
+        output = "";
+        boolean flag = true;
+        if(coffeeDispensed)
+        {
+            flag = false;
+           for(int i = 0; i < numCoffees; i++)
+           {
+               output += "coffee ";
+           }
+           coffeeDispensed = false;
+           numCoffees = 0;
+        }
+        if(changeButton)
+        {
+            flag = false;
+            output += ("Change " + changeReturned);
+            changeReturned = "";
+        }
+        if(flag)
+        {
+            output += "Nothing";
+        }
+        return output;
+    }
+
+    private void delta(int q, int n, int d, boolean buttonPressed)
+    {
+
         quarters += q;
         nickels += n;
         dimes += d;
         changeButton = buttonPressed;
-        double totalInserted = (q * .25) + (n * .05) + (d * .1);
-
-        lambda();
-        delta();
-
-    }
-
-    private void lambda()
-    {
+        totalInserted += (q * .25) + (n * .05) + (d * .1);
         if(totalInserted >= 1.00)
         {
-            while(totalInserted > 1.00)
+            coffeeDispensed = true;
+            while(totalInserted >= 1)
             {
-                System.out.println("coffee");
-                totalInserted -= 1.00;
+                numCoffees++;
+                totalInserted -= 1;
             }
         }
-
+        if(changeButton) {
+            try {
+                change();
+            } catch (SimulationException e) {
+                System.out.println("Insufficient change please contact management.");
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
     }
 
-    private void delta()
+    private void change() throws SimulationException
     {
+        double change = totalInserted % 100;
+        BigDecimal bd = new BigDecimal(change).setScale(2, RoundingMode.HALF_UP);
+        change = bd.doubleValue();
+        double numQ = 0, numD = 0, numN = 0;
+        if(change % .25 == 0)
+        {
+            numQ = change / .25;
+        }
+        if(numQ > quarters)
+        {
+            throw new SimulationException("Quarters");
+        }
+        change = change - (numQ * .25);
+        if(change % .10 == 0)
+        {
+            numD = change / .10;
+        }
+        if(numD > dimes)
+        {
+            throw new SimulationException("Dimes");
+        }
+        change = change - (numD * .10);
+        if(change % .05 == 0)
+        {
+            numN = change /.05;
+        }
+        if(numN > nickels)
+        {
+            throw new SimulationException("Nickels");
+        }
+        change = change - (numN * .05);
+        if(change != 0)
+        {
+            throw new SimulationException("Incorrect Change");
+        }
 
-    }
+        for(int i = 0; i < numQ; i++)
+        {
+            changeReturned += 'q';
+        }
 
+        for(int i = 0; i < numD; i++)
+        {
+            changeReturned += 'd';
+        }
 
-    public int getQuarters() {
-        return quarters;
-    }
-
-    public void setQuarters(int quarters) {
-        this.quarters = quarters;
-    }
-
-    public int getNickels() {
-        return nickels;
-    }
-
-    public void setNickels(int nickels) {
-        this.nickels = nickels;
-    }
-
-    public int getDimes() {
-        return dimes;
-    }
-
-    public void setDimes(int dimes) {
-        this.dimes = dimes;
-    }
-
-    public int getTotal() {
-        return total;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
-
-    public boolean isChangeButton() {
-        return changeButton;
-    }
-
-    public void setChangeButton(boolean changeButton) {
-        this.changeButton = changeButton;
+        for(int i = 0; i < numN; i++)
+        {
+            changeReturned += 'n';
+        }
     }
 }
