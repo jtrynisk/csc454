8115c3f08d006697df5a5f0c6e762f9421a030bd
Jondn Tryniski <jtrynisk@oswego.edu>
2019-10-15 11:36:54 -0400

    Start of project 3 code

---
+++ Project 3/src/Framework/Model.java
@@ -0,0 +1,10 @@
+package Framework;
+
+public interface Model<I, O>
+{
+
+    Port<I> port;
+    void Lambda();
+
+    void Delta();
+}
---
+++ Project 3/src/Framework/Port.java
@@ -0,0 +1,21 @@
+package Framework;
+
+public class Port
+{
+    int data;
+
+    public Port()
+    {
+
+    }
+
+    public int getData()
+    {
+        return this.data;
+    }
+
+    public void setData(int data)
+    {
+        this.data = data;
+    }
+}
